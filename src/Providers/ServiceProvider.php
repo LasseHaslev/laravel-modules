576a7dc45a653aa17a07bcd534540a70728c4f19
Lasse S. Haslev <lasse@haslev.no>
2016-12-11 16:25:11 +0100

    Added registerAll method to register

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -17,6 +17,7 @@ class ServiceProvider extends BaseServiceProvider
     public function register()
     {
         $this->mergeConfigFrom( __DIR__.'/../../config/modules.php', 'modules');
+        Modules::registerAll();
     }
 
     /**
